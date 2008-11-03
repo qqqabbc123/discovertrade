a906708eb92fbc7a25de9e3ebcb1ecb010a28c2d
kushalnrp <kushalnrp@e77c96a6-a9da-11dd-a714-1752b6e4c161>
2008-11-03 19:55:10 +0000

    bug fixed. out put was showing same values all the time.

--- trunk/test/test/QuoteFetcher.java
+++ trunk/test/test/QuoteFetcher.java
@@ -7,14 +7,15 @@ import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Map;
 import java.util.TreeMap;
 
 import org.discover.trading.core.IQuote;
+import org.discover.trading.core.LiveQuote;
 import org.discover.trading.core.RegexQuoteTemplate;
 import org.discover.trading.core.Security;
+import org.discover.trading.core.SimpleQuote;
 import org.discover.trading.io.CSVQuoteWriter;
 import org.discover.trading.io.IQuoteWriter;
 import org.discover.trading.io.TextQuoteReader;
@@ -44,33 +45,36 @@ public class QuoteFetcher {
 					+ "\\s*+" + tagStart + value + tagEnd);
 		}
 	}
-	
+
 	static void run() {
 
 		// If market is not yet open just sleep over
 		while (!isMartketOpen()) {
 			try {
-				System.out.println("Market not open will wait for " + waitTime / 1000 + " seconds");
+				System.out.println("Market not open will wait for " + waitTime
+						/ 1000 + " seconds");
 				Thread.sleep(waitTime);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		// Create a new csv writer to write all the data.
 		PrintWriter quoteWriter = null;
 		try {
-			String[] headers = new String[] {"Date", "Open", "High", "Low", "Close", "Volume"};
-			quoteWriter = new PrintWriter(new FileWriter(stockSymbol + " " + Calendar.getInstance().getTime().toString()));
+			String[] headers = new String[] { "Date", "Open", "High", "Low",
+					"Close", "Volume" };
+			quoteWriter = new PrintWriter(new FileWriter(stockSymbol + ".csv"));
 		} catch (IOException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
-		
+
 		// previous quote.
-		TextQuoteReader quoteReader = new TextQuoteReader(new Security(),
-				null, iciciDirectTemplate);
+		TextQuoteReader quoteReader = new TextQuoteReader(new Security(), null,
+				iciciDirectTemplate);
+		LiveQuote prevQuote = null;
 		while (!isMarketClosed()) {
 			BufferedInputStream reader = null;
 			try {
@@ -90,15 +94,35 @@ public class QuoteFetcher {
 			try {
 				while ((c = reader.read()) != -1)
 					source = source + (char) c;
-				
+
 				try {
-					IQuote quote = quoteReader.read(source);
-					quoteWriter.println("");
-					if (quote != null) {
-						quoteWriter.println(quote);
-						quoteWriter.flush();
+					LiveQuote currQuote = (LiveQuote) quoteReader.read(source);
+					if (currQuote != null) {
+						SimpleQuote quote = new SimpleQuote();
+						if (prevQuote == null) {
+						} else {
+							quote.setDate(currQuote.getLastTradedTime());
+							quote.setOpen(prevQuote.getLastTradedPrice());
+							quote.setClose(currQuote.getLastTradedPrice());
+							quote
+									.setHigh(prevQuote.getLastTradedPrice() > currQuote
+											.getLastTradedPrice() ? prevQuote
+											.getLastTradedPrice() : currQuote
+											.getLastTradedPrice());
+							quote
+									.setLow(prevQuote.getLastTradedPrice() < currQuote
+											.getLastTradedPrice() ? prevQuote
+											.getLastTradedPrice() : currQuote
+											.getLastTradedPrice());
+							quote.setVolume(currQuote.getVolume()
+									- prevQuote.getVolume());
+							quoteWriter.println(quote);
+							System.out.println(quote);
+							quoteWriter.flush();
+						}
+						prevQuote = currQuote;
 					}
-					System.out.println(quote);
+					// System.out.println(quote);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -114,9 +138,9 @@ public class QuoteFetcher {
 					e.printStackTrace();
 				}
 			}
-			
+
 			try {
-				Thread.sleep(10000);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -134,40 +158,40 @@ public class QuoteFetcher {
 	}
 
 	private static boolean isMarketClosed() {
-		int hour   = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+		int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
 		int minute = Calendar.getInstance().get(Calendar.MINUTE);
-		
+
 		if ((hour == 15 && minute >= 35) || hour > 15)
 			return true;
-		else 
+		else
 			return false;
 	}
 
 	private static boolean isMartketOpen() {
 		// Start time.
-		int hour   = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+		int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
 		int minute = Calendar.getInstance().get(Calendar.MINUTE);
-		int day    = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);	
-		
-		if (day == Calendar.SATURDAY || day == Calendar.SUNDAY ) {
-			waitTime = 8 * 60 * 60 * 1000; 
+		int day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+
+		if (day == Calendar.SATURDAY || day == Calendar.SUNDAY) {
+			waitTime = 8 * 60 * 60 * 1000;
 			return false;
 		}
-		
+
 		// If the hour is less than 9 o clock
 		if (hour < 9 || hour > 16) {
-			waitTime = 50 * 60 * 1000; 
+			waitTime = 50 * 60 * 1000;
 			return false;
 		}
-		
+
 		else if ((hour == 9 && minute >= 50) || hour > 9)
 			return true;
-		
+
 		else {
 			waitTime = 10 * 1000;
 			return false;
 		}
-		
+
 	}
 
 }
